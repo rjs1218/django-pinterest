9d538a9925d8c517f4b7ec5c61d7d2b791474009
rjs1218 <95990765+rjs1218@users.noreply.github.com>
2022-05-19 16:57:48 +0900

    django 26 course commit

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from django.contrib.auth.views import LoginView, LogoutView
-from accountapp.views import AccountCreateView, AccountDetailView, AccountUpdateView, hello_world
+from accountapp.views import AccountCreateView, AccountDeleteView, AccountDetailView, AccountUpdateView, hello_world
 
 app_name = "accountapp"
 
@@ -14,4 +14,5 @@ urlpatterns = [
     path('create/', AccountCreateView.as_view(), name='create'), 
     path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'), 
     path('update/<int:pk>', AccountUpdateView.as_view(), name='update'), 
+    path('delete/<int:pk>', AccountDeleteView.as_view(), name='delete'), 
 ]
