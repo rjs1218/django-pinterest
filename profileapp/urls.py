670368ac7fa5dd6bb023c35f488a47698cc90ca5
rjs1218 <95990765+rjs1218@users.noreply.github.com>
2022-05-23 14:10:03 +0900

    django 31 course commit

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -1,7 +1,11 @@
+from django.urls import path
+
+from profileapp.views import ProfileCreateView
+
 
 app_name = 'profileapp'
 
-urlpatterns = [
-
+urlpatterns = [ 
+    path('create/', ProfileCreateView.as_view(), name='create')
 
 ]
